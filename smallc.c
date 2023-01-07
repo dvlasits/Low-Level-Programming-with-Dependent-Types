0c87132fea88992617b90a0b037db217d5f0e05c
danielvlasits <dvlasits@gmail.com>
2023-01-07 12:57:31 +0100

    FFI c file

--- smallc.c
+++ smallc.c
@@ -1,32 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct
-{
-    int *ptr;
-    int num;
-} pointnum;
-
-int *getPointer(void)
-{
-    int *a = malloc(sizeof(int));
-    fprintf(stderr, "Getting a new pointer %d\n", a);
-    return a;
-}
+// Creating Array
 
 int *getArray(int size)
 {
     return malloc(sizeof(int) * size);
 }
 
+int *getArrayChar(int size)
+{
+    fprintf(stderr, "Run create arr %s \n", "size");
+    fprintf(stderr, "Run create arr %d \n", size);
+    return malloc(sizeof(char) * size);
+}
+
+// Reading Array
+
 int readPointerOffset(int offset, int *arr)
 {
     return arr[offset];
 }
 
+char readPointerOffsetChar(int offset, char *arr)
+{
+    return arr[offset];
+}
+
+// Writing Array
+
 void writeOffset(int offset, int *arr, int val)
 {
     arr[offset] = val;
+}
+
+void writeOffsetChar(int offset, char *arr, char val)
+{
+    arr[offset] = val;
+}
+
+// Freeing Array
+void freePointer(void *pt)
+{
+    free(pt);
+}
+
+void freePointerChar(char *pt)
+{
+    free(pt);
+}
+
+// Creating Any Pointers
+void *createStruct(int size)
+{
+    // fprintf(stderr, "%s", "Run create Struct\n", 30);
+    return malloc(size);
+}
+
+// Getting Data from Structs
+
+int getIntStruct(unsigned char *ptr, int offset)
+{
+    unsigned char *ptr2 = ptr + offset;
+    int *ptr3 = (int *)ptr2;
+    return (*ptr3);
+}
+
+char getCharStruct(unsigned char *ptr, int offset)
+{
+    return (ptr[offset]);
+}
+
+void *getStructStruct(unsigned char *ptr, int offset)
+{
+    return (void *)(ptr + offset);
+}
+
+// Writing to Structs
+void writeIntStruct(unsigned char *ptr, int offset, int num)
+{
+    // fprintf(stderr, "Writing %d to offset %d", num, offset, 30);
+    unsigned char *a = ptr + offset;
+    a[3] = (num >> 24) & 0xFF;
+    a[2] = (num >> 16) & 0xFF;
+    a[1] = (num >> 8) & 0xFF;
+    a[0] = num & 0xFF;
+}
+
+void writeCharStruct(unsigned char *ptr, int offset, char val)
+{
+    // fprintf(stderr, "%s", "string format", 30);
+
+    ptr[offset] = val;
+}
+
+void writeStructStruct(unsigned char *ptr, int offset, unsigned char *ptr2, int size)
+{
+
+    memcpy(ptr + offset, ptr2, size);
+}
+
+int *getPointer(void)
+{
+    int *a = malloc(sizeof(int));
+    return a;
 }
 
 int readNumber(int *ptr)
@@ -39,21 +117,10 @@ int testFunc(int (*ptr)(int))
     return ptr(5);
 }
 
-void freePointer(int *pt)
-{
-    fprintf(stderr, "Freeing pointer %d\n", pt);
-    free(pt);
-}
-
 int *writePointer(int *pt, int num)
 {
     *pt = num;
     return pt;
-}
-
-void smartRead(void (*func)(int *, int), int *ptr)
-{
-    func(ptr, *ptr);
 }
 
 int basicRead(int *ptr)
